13cc34e2b4bb14480858033320d276a9a0cbeafe
Haoran Xu <45837851+fe1ixxu@users.noreply.github.com>
2021-09-06 23:39:36 +0800

    Create generate-wmt.sh

---
+++ generate-wmt.sh
@@ -0,0 +1,9 @@
+DATAPATH=./download_prepare/wmt-data/
+STPATH=${DATAPATH}en-de-databin/
+MODELPATH=./models/one-way-wmt/ 
+PRE_SRC=Coran/bibert-ende
+PRE=Coran/bibert-ende
+CUDA_VISIBLE_DEVICES=0 fairseq-generate \
+${STPATH} --path ${MODELPATH}checkpoint_best.pt --bpe bert --pretrained_bpe ${PRE} --pretrained_bpe_src ${PRE_SRC} \
+--beam 4 --lenpen 0.6 --remove-bpe --vocab_file=${STPATH}/dict.de.txt \
+--max-len-a 1 --max-len-b 50|tee ${STPATH}/generate.out
