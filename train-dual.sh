172903f8f5754b745f72016225f8e34ecce1859a
Haoran Xu <45837851+fe1ixxu@users.noreply.github.com>
2021-09-06 23:49:03 +0800

    Update train-dual.sh

--- train-dual.sh
+++ train-dual.sh
@@ -17,6 +17,6 @@ SAVE_DIR=./models/dual-ft/
 CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train ${TEXT}de-en-databin/ --arch transformer_iwslt_de_en --ddp-backend no_c10d --optimizer adam --adam-betas '(0.9, 0.98)' \
 --clip-norm 1.0 --lr 0.0004 --lr-scheduler inverse_sqrt --warmup-updates 1000 --warmup-init-lr 1e-07 --dropout 0.3 \
 --weight-decay 0.00002 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 2048 --update-freq 4 \
---attention-dropout 0.1 --activation-dropout 0.1 --max-update 25000 --save-dir ${SAVE_DIR}  --encoder-embed-dim 768 --decoder-embed-dim 768 \
+--attention-dropout 0.1 --activation-dropout 0.1 --max-update 5000 --save-dir ${SAVE_DIR}  --encoder-embed-dim 768 --decoder-embed-dim 768 \
 --no-epoch-checkpoints --save-interval 5 --pretrained_model Coran/bibert-ende --use_drop_embedding 8 \
 --restore-file ./models/dual/checkpoint_best.pt --reset-lr-scheduler --reset-dataloader --reset-meters 
