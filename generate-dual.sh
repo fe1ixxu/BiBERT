54aeade5a598bab1921f6ef4a4e11b7d9455c118
Haoran Xu <45837851+fe1ixxu@users.noreply.github.com>
2021-09-06 18:23:22 +0800

    Create generate-dual.sh

---
+++ generate-dual.sh
@@ -0,0 +1,9 @@
+DATAPATH=./download_prepare/data_mixed_ft/
+STPATH=${DATAPATH}de-en-databin/
+MODELPATH=./models/dual-ft/ 
+PRE_SRC=Coran/bibert-ende
+PRE=./download_prepare/8k-vocab-models/
+CUDA_VISIBLE_DEVICES=0 fairseq-generate \
+${STPATH} --path ${MODELPATH}checkpoint_best.pt --bpe bert --pretrained_bpe ${PRE} --pretrained_bpe_src ${PRE_SRC} \
+--beam 4 --lenpen 0.6 --remove-bpe --vocab_file=${STPATH}/dict.en.txt \
+--max-len-a 1 --max-len-b 50|tee ${STPATH}/generate.out
