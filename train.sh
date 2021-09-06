43dbbc426ab7c8d3014045ecd3217730ccb4c783
Haoran Xu <45837851+fe1ixxu@users.noreply.github.com>
2021-09-06 17:28:37 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -5,4 +5,4 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train ${TEXT}de-en-databin/ --arch transfor
 --clip-norm 1.0 --lr 0.0004 --lr-scheduler inverse_sqrt --warmup-updates 1000 --warmup-init-lr 1e-07 --dropout 0.3 \
 --weight-decay 0.00002 --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --max-tokens 2048 --update-freq 4 \
 --attention-dropout 0.1 --activation-dropout 0.1 --max-update 10000 --save-dir ${SAVE_DIR}  --encoder-embed-dim 768 --decoder-embed-dim 768 \
---no-epoch-checkpoints --save-interval 5 --pretrained_model Coran/bibert-ende --use_drop_embedding 8  #-distributed-world-size 4
+--no-epoch-checkpoints --save-interval 5 --pretrained_model Coran/bibert-ende --use_drop_embedding 8 
