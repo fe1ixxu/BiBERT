54346f179885053693483a38e8125da3b3fb068d
Haoran Xu <45837851+fe1ixxu@users.noreply.github.com>
2021-09-07 19:39:12 +0800

    Update train-wmt.sh

--- train-wmt.sh
+++ train-wmt.sh
@@ -1,5 +1,5 @@
 TEXT=./download_prepare/wmt-data/
-SAVE_DIR=../models/one-way-wmt/
+SAVE_DIR=./models/one-way-wmt/
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train ${TEXT}en-de-databin/ --arch transformer_vaswani_wmt_en_de_big --ddp-backend no_c10d --optimizer adam \
 --adam-betas '(0.9, 0.98)' --clip-norm 1.0 --lr 0.001 --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-07 --dropout 0.3 \
